d9a6cbf62bd1d9eba6df1291d66b9e4d062c2dbf
PhysRex <ThePhysRex@gmail.com>
2018-05-27 19:18:46 -0400

    adding dist files

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,126 @@
+const _ = require('lodash');
+const dealerJSON = require('../../dealers.json');
+
+
+const data = {
+  read(url) {
+    const request = new XMLHttpRequest();
+    request.overrideMimeType('application/json');
+    request.open('GET', url, true);
+    request.onreadystatechange = function () {
+      if (request.readyState == 4 && request.status == '200') {
+        // .open will NOT return a value but simply returns undefined in async mode so use a callback
+
+        // raw JSON response from server request
+        const data = request.responseText;
+
+        // parsed JSON response -> javascript object
+        const dataObj = JSON.parse(data);
+        console.log('dataObj', dataObj);
+      }
+    };
+    request.send(null);
+  },
+  get() {
+    return fetch('./dealers.json')
+      .then(response => response.json());
+  },
+  parse: {
+    weekHours({ mon, sat, sun }) {
+      return {
+        Weekdays: mon,
+        Saturdays: (sat === '') ? ' - CLOSED' : (sat === 'On Call') ? ' - On Call' : sat,
+        Sundays: (sun === '') ? ' - CLOSED' : (sun === 'On Call') ? ' - On Call' : sun,
+      };
+    },
+    filterDealers(dealerList, filters) {
+      return dealerList.filter(({ data }) => data.certifications.some(cert => filters.includes(cert)));
+    },
+  },
+};
+
+
+const useful = {
+  truncateString(str, num) {
+    // Clear out that junk in your trunk
+
+    if (str.length <= num) {
+      return str;
+    } else if (num <= 3) {
+      str = `${str.slice(0, num)}...`;
+    } else {
+      str = `${str.slice(0, num - 3)}...`;
+    }
+
+    return str;
+  },
+  textCheckAdd(str, strToCheck) {
+    // var expression1 = /(http|\/\/)/i;
+    const expression1 = new RegExp(strToCheck, 'i');
+    if (!str.match(expression1)) {
+      return str = `${strToCheck}://${str}`;
+    }
+    return str;
+  },
+  textCheckRemove(str, strToCheck) {
+    // var expression1 = /(http|\/\/)/i;
+    const expression1 = new RegExp(strToCheck, 'i');
+    return str.replace(expression1, '');
+  },
+  regexEscape(str) {
+    // function to escape special characters when not done properly
+    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  },
+  padZero(num) {
+    num = parseInt(num);
+    if (num < 10) {
+      num = `0${num}`;
+    }
+    return num;
+  },
+  padDecimals(num) {
+    num = parseInt(num);
+    if (num === Math.floor(num)) {
+      num += '.00';
+    }
+    return num;
+  },
+  time() {
+    // returns current time in hrs:min:sec (24-hour format)
+
+    const date = new Date();
+    const hrs = this.padZero(date.getHours());
+    const min = this.padZero(date.getMinutes());
+    const sec = this.padZero(date.getSeconds());
+
+    const currentTime = `${hrs}:${min}:${sec}`;
+    return currentTime;
+  },
+  objectSort(property) {
+    let sortOrder = 1;
+    if (property[0] === '-') {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a, b) {
+      const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+    };
+  },
+  scrollToElement(element) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+    });
+  },
+  scrollToPosition(y) {
+    document.body.scrollIntoView({
+      y,
+      block: 'start',
+      behavior: 'smooth',
+    });
+  },
+};
+
 
 const handler = {
   contactPro() {
@@ -51,7 +174,7 @@ const handler = {
 
       // add listener to filter icon
       handler.elements.filterIcon.addEventListener('click', actions.toggleFilter);
-      
+
       // modal input validation listeners
       [...handler.modalInput].forEach(pro => (pro).addEventListener('keyup', actions.modalValidation));
 
@@ -65,22 +188,22 @@ const handler = {
       [...handler.contactPro()].forEach(pro => (pro).addEventListener('click', actions.openModal));
     },
   },
-}
+};
 
 
 const actions = {
   openModal(event) {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    document.body.classList.add("overflow-y-hidden");
-    handler.elements.modal.style.display = "block";
+    document.body.classList.add('overflow-y-hidden');
+    handler.elements.modal.style.display = 'block';
 
     // set modal title
     actions.modalTitle(event.currentTarget.dataset.title);
   },
   closeModal(event) {
-    document.body.classList.remove("overflow-y-hidden");    
-    handler.elements.modal.style.display = "none";
+    document.body.classList.remove('overflow-y-hidden');
+    handler.elements.modal.style.display = 'none';
   },
   closeOnlyModal(event) {
     if (event.target.id === 'poolModal') actions.closeModal();
@@ -93,7 +216,7 @@ const actions = {
     // change validation image
     if (target.value.length > 0) {
       inputImg.src = 'src/assets/checkmark-circle.png';
-      target.classList.remove('modal-input-border-wrong');      
+      target.classList.remove('modal-input-border-wrong');
     } else {
       inputImg.src = 'src/assets/circle-form.png';
       target.classList.add('modal-input-border-wrong');
@@ -108,17 +231,17 @@ const actions = {
     });
   },
   modalSend(event) {
-    //check validation
+    // check validation
     const imgs = [...handler.modalImgs].map(images => images.src);
     const imgsTest = imgs.every(src => src.includes('checkmark-circle'));
     if ([...handler.modalImgs].map(images => images.src).every(src => src.includes('checkmark-circle'))) {
       actions.closeModal();
-        
+
       // clear modal input, reset images
       [...handler.modalInput, handler.modalComments].forEach(input => input.value = '');
       [...handler.modalImgs].map(images => images.src = 'src/assets/circle-form.png');
     } else {
-      [...handler.modalImgs].map(image => {
+      [...handler.modalImgs].map((image) => {
         if (image.src.includes('circle-form')) {
           image.parentElement.nextElementSibling.classList.add('modal-input-border-wrong');
         }
@@ -131,7 +254,7 @@ const actions = {
     handler.elements.mainHeader.classList.add('hide');
   },
   toggleNav() {
-    handler.elements.navMobile.classList.toggle("hide");
+    handler.elements.navMobile.classList.toggle('hide');
     handler.elements.search.firstElementChild.classList.add('hide-visibility');
     handler.elements.mainSection.classList.add('main-bg');
     handler.elements.mainHeader.classList.remove('hide');
@@ -156,7 +279,7 @@ const actions = {
     if (data) elem.dataset.title = data;
     if ((typeof content) === 'string') {
       elem.innerHTML = content;
-    } else if ((typeof content) == 'object') {
+    } else if ((typeof content) === 'object') {
       content.forEach(val => elem.appendChild(val));
     } else {
       elem.appendChild(content);
@@ -165,7 +288,7 @@ const actions = {
     return elem;
   },
   makeCard(info) {
-    const {      
+    const {
       companyID,
       name,
       phone1,
@@ -181,14 +304,14 @@ const actions = {
     } = info;
 
     const certificationImages = {
-      "Installation Pro": "src/assets/star-installation-pro.png",
-      "Residential Pro": "src/assets/home-residential-pro.png",
-      "Commercial Pro": "src/assets/users-commercial-pro.png",
-      "Service Pro": "src/assets/gear-service-pro.png",
-    }
+      'Installation Pro': 'src/assets/star-installation-pro.png',
+      'Residential Pro': 'src/assets/home-residential-pro.png',
+      'Commercial Pro': 'src/assets/users-commercial-pro.png',
+      'Service Pro': 'src/assets/gear-service-pro.png',
+    };
 
 
-    //create header for result card
+    // create header for result card
     const header = actions.makeElement({
       element: 'header',
       classNames: ['margin-left-right-25', 'padding-top-bottom-20', 'text-center', 'flex', 'flex-center', 'result-header'],
@@ -246,13 +369,11 @@ const actions = {
     });
 
     const parsedHours = data.parse.weekHours(weekHours);
-    const sectionBottomLis = Object.keys(parsedHours).map(
-      (day) => actions.makeElement({
-        element: 'li',
-        classNames: ['margin-top-10', 'OpenSans-Regular', 'result-hours-item'],
-        content:`${day} ${parsedHours[day]}`,
-      })
-    );
+    const sectionBottomLis = Object.keys(parsedHours).map(day => actions.makeElement({
+      element: 'li',
+      classNames: ['margin-top-10', 'OpenSans-Regular', 'result-hours-item'],
+      content: `${day} ${parsedHours[day]}`,
+    }));
     const sectionBottomDiv = actions.makeElement({
       element: 'div',
       classNames: ['margin-top-bottom-15'],
@@ -266,7 +387,7 @@ const actions = {
           element: 'ul',
           content: sectionBottomLis,
         }),
-      ],  
+      ],
     });
 
     const section = actions.makeElement({
@@ -276,23 +397,21 @@ const actions = {
     });
 
     // create footer for result card
-    const footerLis = certifications.map(
-      (cert) => actions.makeElement({
-        element: 'li',
-        classNames: ['padding-5', 'flex', 'flex-start', 'OpenSans-Regular', 'result-footer-item'],
-        content: [
-          actions.makeElement({
-            element: 'img',
-            classNames: ['padding-right-10', 'xs-icon'],
-            src: certificationImages[cert],
-          }),
-          actions.makeElement({
-            element: 'p',
-            content: cert,
-          }),
-        ],
-      })
-    );
+    const footerLis = certifications.map(cert => actions.makeElement({
+      element: 'li',
+      classNames: ['padding-5', 'flex', 'flex-start', 'OpenSans-Regular', 'result-footer-item'],
+      content: [
+        actions.makeElement({
+          element: 'img',
+          classNames: ['padding-right-10', 'xs-icon'],
+          src: certificationImages[cert],
+        }),
+        actions.makeElement({
+          element: 'p',
+          content: cert,
+        }),
+      ],
+    }));
     const footerUl = actions.makeElement({
       element: 'footer',
       classNames: ['padding-20', 'flex', 'flex-start', 'flex-direction-column', 'flex-wrap', 'result-footer'],
@@ -321,12 +440,20 @@ const actions = {
     let filteredDealers = [];
     if (clickedBoxes.length !== 0) {
       // get filtered list of dealers
+
+      let dealerData;
+      try {
+        dealerData = await data.get();
+      } catch (err) {
+        console.error(`Received AJAX error, defaulting to backup.\n${err}`);
+        dealerData = dealerJSON;
+      }
+
       const {
         zipcode,
         dealers,
-      } = await data.get();
-
-      const dealerList = dealers;
+      } = dealerData;
+      const dealerList = dealers || dealerJSON;
       filteredDealers = data.parse.filterDealers(dealerList, clickedBoxes);
 
       // change filter/search bar message
@@ -357,130 +484,7 @@ const actions = {
   },
   toggleFilter() {
     handler.elements.fieldset.classList.toggle('filter-hide');
-  }
-};
-
-
-const data = {
-  read: function () {
-    var request = new XMLHttpRequest();
-    request.overrideMimeType("application/json");
-    request.open('GET', './code-test.json', true);
-    request.onreadystatechange = function() {
-      if (request.readyState == 4 && request.status == "200") {
-        // .open will NOT return a value but simply returns undefined in async mode so use a callback
-        
-        // raw JSON response from server request
-        var data = request.responseText;
-
-        // parsed JSON response -> javascript object
-        var dataObj = JSON.parse(data)
-
-        actions.makeTable(dataObj);
-      }
-    }
-    request.send(null);
-  },
-  get() {
-    return fetch('./dealers.json')
-      .then(response => response.json());
-  },
-  parse: {
-    weekHours({mon, sat, sun}) {
-      return {
-        Weekdays: mon,
-        Saturdays: (sat === '') ? ' - CLOSED' : (sat === 'On Call') ? ' - On Call' : sat,
-        Sundays: (sun === '') ? ' - CLOSED' : (sun === 'On Call') ? ' - On Call' : sun,
-      };
-    },
-    filterDealers(dealerList, filters) {
-      return dealerList.filter(({ data }) => data.certifications.some(cert => filters.includes(cert)));
-    },
   },
 };
-
-
-const useful = {
-  truncateString: function(str, num) {
-    // Clear out that junk in your trunk
-    
-    if (str.length <= num ) {
-      return str; 
-    } else if (num <= 3) {
-      str = str.slice(0,num) + "...";
-    } else {
-      str = str.slice(0,num-3) + "...";
-    }
-    
-    return str;
-  },
-  textCheckAdd: function(str,strToCheck) {
-    // var expression1 = /(http|\/\/)/i;
-    var expression1 = new RegExp(strToCheck, "i");
-    if ( !str.match(expression1) ) {
-      return str = strToCheck + "://" + str;
-    } else {
-      return str;
-    }
-  },
-  textCheckRemove: function(str,strToCheck) {
-    // var expression1 = /(http|\/\/)/i;
-    var expression1 = new RegExp(strToCheck, "i");
-    return str.replace(expression1,"");
-  },
-  regexEscape: function(str) {
-    // function to escape special characters when not done properly
-    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-  },
-	padZero: function(num) {
-		num = parseInt(num);
-    if (num < 10) {
-			num = "0" + num;
-    }
-    return num;
-	},
-	padDecimals: function(num) {
-		num = parseInt(num);
-    if (num === Math.floor(num)) {
-			num = num + ".00" ;
-    }
-    return num;
-	},
-	time: function() {
-		// returns current time in hrs:min:sec (24-hour format)
-		
-    var date = new Date();
-    var hrs = this.padZero(date.getHours());
-    var min = this.padZero(date.getMinutes());
-    var sec = this.padZero(date.getSeconds());
-		
-		var currentTime = hrs + ":" + min + ":" + sec;
-    return currentTime;
-  },
-  objectSort: function (property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
-    }
-  },
-  scrollToElement(element) {
-    element.scrollIntoView({
-      behavior: 'smooth'
-    });
-  },
-  scrollToPosition(y) {
-    document.body.scrollIntoView({
-      y,
-      block: 'start',
-      behavior: 'smooth'
-    });
-  }
-};
-
 
 handler.listeners.initialize();
