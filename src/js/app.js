1498903b769160e270496e7ed8a250924c20b2d3
PhysRex <ThePhysRex@gmail.com>
2018-05-27 19:33:00 -0400

    styling changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
-const _ = require('lodash');
-const dealerJSON = require('../../dealers.json');
+
+try {
+  var dealerJSON = require('../../dealers.json');
+} catch (e) {
+  // log error here...
+}
 
 
 const data = {
